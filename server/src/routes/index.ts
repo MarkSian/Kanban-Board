230454fe5f822bed46cc608aa3b5f221296d8123
MarkSian <merksianipar@gmail.com>
2025-02-03 15:09:20 -0700

    Update index.ts

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -7,6 +7,8 @@ const router = Router();
 
 router.use('/auth', authRoutes);
 // TODO: Add authentication to the API routes
+router.use('/api', authenticateToken, apiRoutes);
 router.use('/api', apiRoutes);
+
 
 export default router;

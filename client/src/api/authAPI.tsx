64cba9dc67a1daf551f0f0e2db9c6958d8be36e0
MarkSian <merksianipar@gmail.com>
2025-02-03 15:22:38 -0700

    Update authAPI.tsx
    Next complete auth.ts in util folder

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,7 +2,27 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
-}
+  try{
+    const response = await fetch('/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInfo),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error('User not retrieved, check network tab');
+    }
+
+    return data;
+  } catch (err) {
+    console.log('Error from login: ', err);
+    return Promise.reject('Could not get user information');
+  }
+};  
 
 
 
